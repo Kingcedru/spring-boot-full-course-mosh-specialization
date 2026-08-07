4291fb7298607e5c5015cf8119251311cf3e7c95
kingcedru <byishimocedrick@gmail.com>
2026-08-07 09:31:37 +0200

    feat: controlling the bean selection

--- src/main/java/com/springbootcedrick/store/OrderService.java
+++ src/main/java/com/springbootcedrick/store/OrderService.java
@@ -2,6 +2,7 @@ package com.springbootcedrick.store;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +10,8 @@ import org.springframework.stereotype.Service;
 public class OrderService {
     private PaymentService paymentService;
 
-    @Autowired
-    public OrderService(PaymentService paymentService){
+    //@Autowired
+    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
         this.paymentService = paymentService;
     }
     public void placeOrder(){
