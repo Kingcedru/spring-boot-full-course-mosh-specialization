5ee599a86877d5d250ee856d71ddeaeddedcbe3f
kingcedru <byishimocedrick@gmail.com>
2026-08-07 08:39:26 +0200

    feat: constructor injection and interface implementation

--- src/main/java/com/springbootcedrick/store/OrderService.java
+++ src/main/java/com/springbootcedrick/store/OrderService.java
@@ -1,8 +1,16 @@
 package com.springbootcedrick.store;
 
 public class OrderService {
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
     public void placeOrder(){
-        var paymentService =  new StripePaymentService();
         paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService){
+        this.paymentService = paymentService;
     }
 }
